ce9f64190c5b94b711ed1cda33af34a439aa94c9
Stjepan Radičev <stjepan246@gmail.com>
2024-05-13 08:42:22 +0200

    new01;

---
+++ Models/Korisnik.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BiciklistickiKlub.Models
+{
+    [Table("korisnici")]
+    public class Korisnik
+    {
+        public string KorisničkoIme { get; set; }
+        public string Email { get; set; }
+        public string Lozinka { get; set; }
+        public string Prezime { get; set; }
+        public string Ime { get; set; }
+
+        public string PrezimeIme
+        {
+            get
+            {
+                return Prezime + " " + Ime;
+            }
+        }
+
+        public string SifraOvlasti { get; set; }
+        public virtual Ovlast Ovlas { get; set; }
+        public string LozinkaUnos { get; set; }
+        public string LozinkaUnos2 { get; set; }
+    }
+}
